be1ce87fbccd591114e45c91fbcf265fac909d16
Drew Stinnett <drew@drewlink.com>
2021-01-12 19:53:05 -0500

    Adding feed file

---
+++ generate.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import yaml
+from podgen import Podcast
+
+
+def parse_args():
+    """Parse the arguments."""
+    parser = argparse.ArgumentParser(
+        description="This is my super sweet script")
+    parser.add_argument("-i",
+                        "--input",
+                        required=True,
+                        help="Input file to read config and episodes from",
+                        type=argparse.FileType('r'))
+    parser.add_argument("-v",
+                        "--verbose",
+                        help="Be verbose",
+                        action="store_true",
+                        dest="verbose")
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    data = yaml.load(args.input.read(), Loader=yaml.FullLoader)
+    print(data)
+    p = Podcast()
+    p.name = data["name"]
+    p.website = data["website"]
+    p.image = data["image"]
+    p.description = data["description"]
+    p.website = "https://example.org"
+    p.explicit = False
+
+    rssfeed = p.rss_str()
+    print(rssfeed)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
